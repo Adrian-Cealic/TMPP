e2fb88989f0919c1e27e571490d31f5791086bdf
Adrian-Cealic <adrian.cealic@gmail.com>
2026-02-03 17:14:42 +0200

    Update binary files and enhance documentation on SOLID principles with concrete examples

--- DeliverySystem/DeliverySystem.Console/obj/Debug/net8.0/DeliverySystem.Console.AssemblyInfo.cs
+++ DeliverySystem/DeliverySystem.Console/obj/Debug/net8.0/DeliverySystem.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliverySystem.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbfc310ee1ef8a3c212130a557207b3fedacd710")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliverySystem.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliverySystem.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
